d600e6d92d6de8b484ef3aa1914cf76df45d73e2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-09 11:21:10 -0300

    first commit

---
+++ src/main/java/br/com/fiap/ms_produto/dto/LojaDTO.java
@@ -0,0 +1,29 @@
+package br.com.fiap.ms_produto.dto;
+
+
+import br.com.fiap.ms_produto.entities.Loja;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LojaDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Nome da loja requerido")
+    @Size(min = 3, max = 100, message = "O nome da loja deve ter entre 3 a 100 caracteres")
+    private String nome;
+
+    public LojaDTO(Loja entity){
+        id = entity.getId();
+        nome = entity.getNome();
+    }
+
+}
